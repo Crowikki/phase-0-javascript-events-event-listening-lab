e76adbe926bc09696e53f9215df3685a0cee681d
+ <+>
2023-11-05 06:32:21 -0600

    KT Javascript events lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    input.addEventListener('click', clickAlert);
 }
+
+addingEventListener();
+// don't forget to call the event listener to make it work
